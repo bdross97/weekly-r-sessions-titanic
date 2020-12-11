4a2664196129f647c3c863b84868b9947e5fdd49
haydenhoopes <hayden.hoopes@aggiemail.usu.edu>
2020-12-11 12:01:24 -0700

    Added regex to find data on Windows machine without each person having to uncomment stuff. Used Erik's code to import.

--- Titanic Project.R
+++ Titanic Project.R
@@ -3,6 +3,13 @@
 library(tidyverse)
 library(janitor)
 
+### Path for Box Titanic Data ###
+# "This should work on Windows machines for getting the data from box without having to uncomment stuff... It's
+#   just an idea, you don't have to use it."    --- Hayden
+titanic <- "Box/Center for Student Analytics/The Data Scouts/Titanic Project/train.csv"
+pattern <- "(?<=(C[:][/])(User[s]{0,1})[/].{0,20}[/])(.*)(\\w+)"
+my_home <- stringr::str_replace(path.expand("~"), pattern, titanic)
+
 ### Each person's "pullin in" code###
 #train <- rio::import("/Users/erik/Box/Center for Student Analytics/The Data Scouts/Titanic Project/train.csv")
 
@@ -16,3 +23,5 @@ summary(train$Age)
 tabyl(train,Sex)
 #class
 tabyl(train,Pclass)
+
+path.expand('~')
